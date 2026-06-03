bbec534f6906f44803913f69454793a1cf452f7a
Abhay Pratap Singh <abhaypratapsingh9007@gmail.com>
2026-06-03 23:14:46 +0530

    Add user details route and update CORS settings

--- BACKEND/src/app.js
+++ BACKEND/src/app.js
@@ -10,10 +10,14 @@ const registerUser = require('./routes/auth.register.routes')
 const loginUser = require('./routes/auth.login.routes')
 const walletAlter = require('./routes/wallet.alter.routes')
 const userHistory = require('./routes/user.history.routes')
+const userDetails = require('./routes/user.details.routes')
 
 app.use(express.json())
 app.use(cookieParser())
-app.use(cors())
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+}))
 
 app.get('/', (req, res) =>{
     res.send("Hello World")
@@ -27,7 +31,7 @@ app.use('/change', walletAlter)
 
 app.use('/user', userHistory)
 
-
+app.use('/user', userDetails)
 
 
 
